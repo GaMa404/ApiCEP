7e69231f6cc97ea6239b2ad3de7314dadbd11644
GaMa404 <luzettileonardo@gmail.com>
2023-03-21 15:52:01 -0300

    continuação EnderecoController

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -17,6 +17,19 @@ abstract class Controller
         exit(json_encode($data));
     }
 
+    public static function setResponseAsJSON($data, $request_status = true)
+    {
+        $response = array('response_data' => $data, 'response_successful' => $request_status);
+
+        header("Acces-Control-Allow-Origin: *");
+        header("Content-type: application/json; charset-utf-8");
+        header("Cache-Control: no-cache, must-revalidate");
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        header("Pragma: public");
+
+        exit(json_encode($data));
+    }
+
     public static function getExceptionAsJSON(Exception $e)
     {
         $exception = 
